e3f03955d9c0f3c9c07d93bb21b4768541109278
Jukka Liimatta <jukka@liimatta.org>
2016-04-28 01:51:12 +0300

    Add simd placeholder.

---
+++ misc/simd.cpp
@@ -0,0 +1,9 @@
+/*
+    MANGO Multimedia Development Platform
+    Copyright (C) 2012-2016 Twilight Finland 3D Oy Ltd. All rights reserved.
+*/
+#include <mango/simd/simd.hpp>
+
+using namespace mango;
+
+// TODO: coming soon :)
